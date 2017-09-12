f8b0a1aa604cf2109ec188b8d5a3f0261d5842de
Sergey Ibragimov <serg20309@gmail.com>
2017-09-12 16:14:01 +0300

    send info file with image path to network

--- tensorflow/predict.py
+++ tensorflow/predict.py
@@ -1,76 +1,91 @@
-import argparse
 import os
+import sys
+
 import numpy as np
+import scipy.misc
 import tensorflow as tf
-from matplotlib import pyplot as plt
 from PIL import Image
+from matplotlib import pyplot as plt
 
 import models
 
-def predict(model_data_path, image_path):
+MODEL_PATH = 'NYU_ResNet-UpProj.npy'
 
+
+def load_network(session, model_data_path):
     # Default input size
-    height = 228
-    width = 304
-    channels = 3
     batch_size = 1
-    
-    # Read image
-    img = Image.open(image_path)
-    img = img.resize([width,height], Image.ANTIALIAS)
-    img = np.array(img).astype('float32')
-    img = np.expand_dims(np.asarray(img), axis = 0)
-   
+    channels = 3
     # Create a placeholder for the input image
     input_node = tf.placeholder(tf.float32, shape=(None, height, width, channels))
-    
+
     # Construct the network
     net = models.ResNet50UpProj({'data': input_node}, batch_size)
-        
-    with tf.Session() as sess:
 
-        # Load the converted parameters
-        print('Loading the model')
-        net.load(model_data_path, sess)      
-        
-        uninitialized_vars = []
-        for var in tf.global_variables():
-            try:
-                sess.run(var)
-            except tf.errors.FailedPreconditionError:               
-                uninitialized_vars.append(var)
+    # Load the converted parameters
+    print('Loading the model')
+    net.load(model_data_path, session)
 
-        init_new_vars_op = tf.variables_initializer(uninitialized_vars)
-        sess.run(init_new_vars_op)
-        
-        # Evalute the network for the given image
-        pred = sess.run(net.get_output(), feed_dict={input_node: img})
-        
-        # Plot result
+    uninitialized_vars = []
+    for var in tf.global_variables():
+        try:
+            session.run(var)
+        except tf.errors.FailedPreconditionError:
+            uninitialized_vars.append(var)
+
+    init_new_vars_op = tf.variables_initializer(uninitialized_vars)
+    session.run(init_new_vars_op)
+
+    def prediction_func(img):
+        return session.run(net.get_output(), feed_dict={input_node: img})
+
+    return prediction_func
+
+
+height = 228
+width = 304
+
+
+def predict(prediction_func, image_path, channel_to_change=2, visualize=False):
+    # Read image
+    input_img = Image.open(image_path)
+    img = input_img.resize([width, height], Image.ANTIALIAS)
+    img = np.array(img).astype('float32')
+    img = np.expand_dims(np.asarray(img), axis=0)
+
+    # Evalute the network for the given image
+    pred = prediction_func(img)
+
+    pred = np.uint8(np.resize(pred, pred.shape[1:3]) * 50)
+    pred_resized = scipy.misc.imresize(pred, input_img.size, interp='cubic')
+    result_img = np.asarray(input_img)
+    result_img.setflags(write=1)
+    result_img[:, :, channel_to_change] = pred_resized
+
+    # Plot result
+    if visualize:
         fig = plt.figure()
-        ii = plt.imshow(pred[0,:,:,0], interpolation='nearest')
+        ii = plt.imshow(pred[:, :], interpolation='nearest')
         fig.colorbar(ii)
         plt.show()
 
-        return pred
-        
-                
-def main():
-    # Parse arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('model_path', help='Converted parameters for the model')
-    parser.add_argument('image_paths', help='Directory of images to predict')
-    args = parser.parse_args()
+    return result_img
 
-    # Predict the image
-    pred = predict(args.model_path, args.image_paths)
-    
+
+def main():
+    info_file_path = sys.argv[1]
+    img_paths = (s.split(' ') for s in open(info_file_path).read().split('\n'))
+
+    with tf.Session() as session:
+        prediction_func = load_network(session, MODEL_PATH)
+
+        # Predict the image
+        for img_path, out_path in img_paths:
+            modified_image = predict(prediction_func, img_path)
+            Image.fromarray(modified_image).save(out_path)
+
     os._exit(0)
+
 
 if __name__ == '__main__':
     main()
-
-        
-
-
-
